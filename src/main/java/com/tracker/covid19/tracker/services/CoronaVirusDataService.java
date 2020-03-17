0a36823bcfa5a2deaf0801ff6a28ac7056bd7626
3Anish <17@Anish>
2020-03-18 04:20:31 +0530

    Initial Commit: Basic Covid-19 Tracker

---
+++ src/main/java/com/tracker/covid19/tracker/services/CoronaVirusDataService.java
@@ -0,0 +1,56 @@
+package com.tracker.covid19.tracker.services;
+
+import com.tracker.covid19.tracker.models.LocationStats;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.io.StringReader;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CoronaVirusDataService {
+    private static String VIRUS_DATA_URL="https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv";
+    private List<LocationStats> allStats=new ArrayList<>();
+
+    public List<LocationStats> getAllStats() {
+        return allStats;
+    }
+
+    @PostConstruct
+    @Scheduled(cron="* * * 1 * *")
+    public void fetchVirusData() throws IOException, InterruptedException {
+        List<LocationStats> newStats=new ArrayList<>();
+        HttpClient client=HttpClient.newHttpClient();
+       HttpRequest request= HttpRequest.newBuilder()
+                .uri(URI.create(VIRUS_DATA_URL))
+               .build();
+      HttpResponse<String> httpResponse= client.send(request, HttpResponse.BodyHandlers.ofString());
+       System.out.println((httpResponse.body()));
+        StringReader csvBodyReader = new StringReader(httpResponse.body());
+        Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(csvBodyReader);
+            for (CSVRecord record : records) {
+                LocationStats locationStats=new LocationStats();
+
+                locationStats.setState(record.get("Province/State"));
+                locationStats.setCountry(record.get("Country/Region"));
+                int latestCases=Integer.parseInt(record.get(record.size()-1));
+                int prevDayCases=Integer.parseInt(record.get(record.size()-2));
+                locationStats.setLatestTotalCases(latestCases);
+                locationStats.setDiffFromPrevDay(latestCases-prevDayCases);
+                System.out.println(locationStats);
+                newStats.add(locationStats);
+
+        }
+            this.allStats=newStats;
+
+    }
+}
